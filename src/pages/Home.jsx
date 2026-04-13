7f26c8b2845db58166d29f0ae4757ee9f7570372
ahmedtika74 <tekaliker@gmail.com>
2026-04-13 20:31:34 +0200

    fix width

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Feed from "../components/Feed";
 export default function Home() {
   return (
     <div className="w-full">
-      <div className="mx-auto mt-5 flex w-full flex-col gap-10">
+      <div className="mx-auto mt-5 flex w-full max-w-4xl flex-col gap-10">
         <CreatePostForm />
         <Feed />
       </div>
